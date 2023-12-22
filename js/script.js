1b26e1126174b45510d6e739e0b08fb1d02ad248
fikri ccifk <muhammadfikri135@gmail.com>
2023-12-22 18:57:48 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 // toggle class active
 
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika huber di click
-document.querySelector('#hamburger-menu');
+document.querySelector("#hamburger-menu");
 onclick = () => {
-    navbarNav.classList.toggle('active');
-} 
+  navbarNav.classList.toggle("active");
+};
 
 // klik diluar side bar untuk menghilangkan bar
 
-const hamburger = document.querySelector
-('hamburger-menu'); 
+const hamburger = document.querySelector("hamburger-menu");
 
-document.addEventListener('click', function(e){
-    if(!hamburger.contains() && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
-})
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains() && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
