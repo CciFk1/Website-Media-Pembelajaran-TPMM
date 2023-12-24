fd384f6983dd9627873de4a0806e42efe054baad
CciFk1 <124134287+CciFk1@users.noreply.github.com>
2023-12-24 16:39:15 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 // toggle class active
 
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika huber di click
-document.querySelector('#hamburger-menu');
-onclick = () => {
-    navbarNav.classList.toggle('active');
-} 
+document.querySelector("#hamburger-menu").onclick = function () {
+  navbarNav.classList.toggle("active");
+};
 
 // klik diluar side bar untuk menghilangkan bar
 
-const hamburger = document.querySelector
-('hamburger-menu'); 
-
-document.addEventListener('click', function(e){
-    if(!hamburger.contains() && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
+const hamburger = document
+  .querySelector("#hamburger-menu")
+  .document.addEventListener("click", function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+      navbarNav.classList.remove("active");
     }
-})
+  });
